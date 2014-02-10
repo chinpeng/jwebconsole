568d4e19b9e687dda8dfdfd3d23d686eaa567207
jet-black <ovechkin.dm@gmail.com>
2014-02-10 15:09:25 +0400

    add tests for server-side validations

--- server/src/test/scala/org/jwebconsole/server/context/host/HostCommandValidatorSpecs.scala
+++ server/src/test/scala/org/jwebconsole/server/context/host/HostCommandValidatorSpecs.scala
@@ -3,9 +3,10 @@ package org.jwebconsole.server.context.host
 import org.specs2.mutable.Specification
 import org.specs2.mock.Mockito
 import org.specs2.time.NoTimeConversions
-import org.jwebconsole.server.util.AkkaTestkitSupport
+import org.jwebconsole.server.util.{InvalidMessage, Invalid, Valid, AkkaTestkitSupport}
 import org.jwebconsole.server.jmx.JMXConnectionChecker
 import akka.actor.Props
+import org.jwebconsole.server.util.ValidationConstants._
 
 class HostCommandValidatorSpecs extends Specification with Mockito with NoTimeConversions {
 
@@ -14,12 +15,60 @@ class HostCommandValidatorSpecs extends Specification with Mockito with NoTimeCo
   trait mocks extends AkkaTestkitSupport {
     val checker = mock[JMXConnectionChecker]
     val validator = system.actorOf(Props(new HostCommandValidator(checker)))
+    var model = HostStateModel("test-id", "localhost", 8080, deleted = false)
+
+    def expectSuccess = expectMsgPF() {
+      case Valid(_) => true
+    }
+
+    def expectFailure(msg: InvalidMessage) = expectMsgPF() {
+      case Invalid(_, items) if items.head.id == msg.id => true
+    }
 
   }
 
   "Host Validator" should {
     "respond with successful validation" in new mocks {
+      validator ! ValidateHost(model, ChangeHostCommand("test-id", "newName", 8080))
+      expectSuccess
+    }
+  }
 
+  "Host Validator" should {
+    "validate negative port" in new mocks {
+      validator ! ValidateHost(model, ChangeHostCommand("test-id", "newName", -8080))
+      expectFailure(PortMustBePositive)
+    }
+  }
+
+  "Host Validator" should {
+    "validate empty port" in new mocks {
+      validator ! ValidateHost(model, ChangeHostCommand("test-id", "newName", 0))
+      expectFailure(PortMustBePositive)
+    }
+  }
+
+  "Host validator" should {
+    "validate empty host" in new mocks {
+      validator ! ValidateHost(model, ChangeHostCommand("test-id", "", 10))
+      expectFailure(HostEmptyMessage)
+    }
+  }
+
+  "Host validator" should {
+    "validate host when it's unreachable" in new mocks {
+      checker.checkConnection(anyString, anyInt) throws new RuntimeException
+      validator ! ValidateHost(model, ChangeHostCommand("test-id", "test", 10))
+      expectFailure(UnableToConnectMessage)
+    }
+  }
+
+  "Host validator" should {
+    "respond with sequence of validations" in new mocks {
+      validator ! ValidateHost(model, ChangeHostCommand("test-id", "", -10))
+      expectMsgPF() {
+        case Invalid(_, msgs) if msgs.size == 2 => true
+      }
     }
   }
 
