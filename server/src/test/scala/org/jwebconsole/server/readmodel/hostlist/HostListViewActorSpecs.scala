8e22f3b408cfc86dd00bb163ea05fa68fd742ed3
jet-black <ovechkin.dm@gmail.com>
2014-02-27 18:47:11 +0400

    add tests for host view actor

--- server/src/test/scala/org/jwebconsole/server/readmodel/hostlist/HostListViewActorSpecs.scala
+++ server/src/test/scala/org/jwebconsole/server/readmodel/hostlist/HostListViewActorSpecs.scala
@@ -4,9 +4,9 @@ import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.mock.Mockito
 import org.specs2.time.NoTimeConversions
 import org.jwebconsole.server.util.AkkaTestkitSupport
-import akka.testkit.TestActorRef
+import akka.testkit.{TestProbe, TestActorRef}
 import akka.actor.Props
-import org.jwebconsole.server.context.host.{HostData, HostDataChangedEvent}
+import org.jwebconsole.server.context.host.{HostDeletedEvent, HostData, HostDataChangedEvent}
 
 class HostListViewActorSpecs extends SpecificationWithJUnit with Mockito with NoTimeConversions {
 
@@ -16,11 +16,25 @@ class HostListViewActorSpecs extends SpecificationWithJUnit with Mockito with No
     val actor: TestActorRef[HostListViewActor] = TestActorRef(Props(new HostListViewActor(dao)))
     val source = actor.underlyingActor
     val hostId = "test-id"
+    val probe = TestProbe()
+    val probeRef = probe.ref
   }
 
   "Host List View actor" should {
     "not replay Host Data Change Events" in new mocks {
       source.filterFunc(HostDataChangedEvent(hostId, HostData())) must beFalse
+    }
+  }
+
+  "Host List View actor" should {
+    "replay host changed events" in new mocks {
+      source.filterFunc(HostDeletedEvent(hostId)) must beTrue
+    }
+  }
+
+  "Host List View actor" should {
+    "fire hosts list event on finish recover" in new mocks {
+
     }
   }
 
